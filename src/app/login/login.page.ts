8efb637091ad145b3db25276f7a0ae50c5b80929
GustavoBatistaLIMA <gustavobatistalima@hotmail.com>
2020-09-05 13:59:55 -0300

    Alteração Pagina Login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -13,3 +14,5 @@ export class LoginPage implements OnInit {
   }
 
 }
+
+
