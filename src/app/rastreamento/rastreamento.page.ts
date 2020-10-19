0934352407acf203c7d1203b9758347059088e60
GustavoBatistaLIMA <gustavobatistalima@hotmail.com>
2020-10-18 22:02:36 -0300

    carregamento páginas chat e rastreamento

---
+++ src/app/rastreamento/rastreamento.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rastreamento',
+  templateUrl: './rastreamento.page.html',
+  styleUrls: ['./rastreamento.page.scss'],
+})
+export class RastreamentoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
