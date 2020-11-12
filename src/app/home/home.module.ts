a37e6f72f168cf22ee978dd31c5bfcee9dce4094
GustavoBatistaLIMA <gustavobatistalima2019@hotmail.com>
2020-11-11 21:44:54 -0300

    estilização login e home

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-
+import { SharedComponentsModule } from './../components/shared-components.module';
 import { HomePageRoutingModule } from './home-routing.module';
 
 
@@ -12,7 +12,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    SharedComponentsModule,
   ],
   declarations: [HomePage]
 })
